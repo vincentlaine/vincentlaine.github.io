5ae1e0c61b408fa5f99654c75b0555ded920d834
lainevin <lainevin@etu.utc.fr>
2016-02-10 13:59:28 +0200

    responsive others

--- js/vince.js
+++ js/vince.js
@@ -8,14 +8,21 @@ function init(){
 
 	$(document).ready(function(){
 		$('.scrollspy').scrollSpy({offset: 100});
-    	$('.modal-trigger').leanModal();
+		$('.modal-trigger').leanModal();
 
-    	var left;
+		var left;
 		left = ($(".header-div").width()/2 - $('#header-info').width()/2);
 		console.log($(".header-div").width()/2);
 		console.log($('#header-info').width()/2);
 		console.log(left);
-	    $('#header-info').css("left", left);
+		$('#header-info').css("left", left);
+
+		if($(window).width() > 1100)
+			$('.div-chart').removeClass().addClass("div-chart col s3");
+		else if ($(window).width() > 400)
+			$('.div-chart').removeClass().addClass("div-chart col s6");
+		else
+			$('.div-chart').removeClass().addClass("div-chart col s12");
 	});
 
 	$(".button-collapse").sideNav();
@@ -27,6 +34,13 @@ window.onresize = function(event) {
 	var left;
 	left = ($(".header-div").width()/2 - $('#header-info').width()/2);
 	$('#header-info').css("left", left);
+
+	if($(window).width() > 1100)
+		$('.div-chart').removeClass().addClass("div-chart col s3");
+	else if ($(window).width() > 400)
+		$('.div-chart').removeClass().addClass("div-chart col s6");
+	else
+		$('.div-chart').removeClass().addClass("div-chart col s12");
 };
 
 window.onscroll = function () {
